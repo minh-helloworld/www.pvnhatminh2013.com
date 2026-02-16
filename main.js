4a46356256a236ec9e048d5b66d3b6c228e91ff4
pvnhatminh2013 <pvnhatminh2013@gmail.com>
2026-02-16 16:21:21 +0700

    Fix image_toggle function to use String constructor

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 // do ya even know when to type words on me
 // huh? f*cking son of a faggot's wanker's ballox's bitch
 function image_toggle() {
-  let img_tag = document.getElementbyId("image").innerHTML;
+  let img_tag = String(document.getElementbyId("image").innerHTML);
   if (img_tag.includes("hidden")) {
     document.getElementbyId("image") = "\n  <img src=\"cat.jpg\" width=\"255\" height=\"255\" style=\"display: block; margin: auto;\">\n";
   }
