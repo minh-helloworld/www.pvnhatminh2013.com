a9220f7aed5a5de168e1b6ef88b4d364ea0ad47a
pvnhatminh2013 <pvnhatminh2013@gmail.com>
2026-02-16 16:35:36 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,9 @@
 function image_toggle() {
   let img_tag = String(document.getElementById("image").innerHTML);
   if (img_tag.includes("hidden")) {
-    document.getElementById("image") = "\n  <img src=\"cat.jpg\" width=\"255\" height=\"255\" style=\"display: block; margin: auto;\">\n";
+    document.getElementById("image").innerHTML = "\n  <img src=\"cat.jpg\" width=\"255\" height=\"255\" style=\"display: block; margin: auto;\">\n";
   }
   else {
-    document.getElementById("image") = "\n  <img src=\"cat.jpg\" width=\"255\" height=\"255\" style=\"display: block; margin: auto; visibility: hidden;\">\n";
+    document.getElementById("image").innerHTML = "\n  <img src=\"cat.jpg\" width=\"255\" height=\"255\" style=\"display: block; margin: auto; visibility: hidden;\">\n";
   }
 }
